372b0d48a736947296deb3a4aef70ef70c4e1fb7
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-08-03 20:47:48 -0300

    Mejorando eficiencia de codigo

--- Ej0.py
+++ Ej0.py
@@ -1,13 +1,13 @@
 smaFast=100
 smaSlow=98
 buyPrice=106
+price=128
 
-tendencia=smaSlow*1.02
-tendenciaBull=smaFast>tendencia
-stopLoss=buyPrice*0.95
-stopWin=buyPrice*1.2
+tendencia=smaFast>smaSlow*1.02
+stopLoss=price<buyPrice*0.95
+stopWin=price>buyPrice*1.2
 
-if (tendenciaBull==True and buyPrice>stopLoss and buyPrice<=stopWin):
+if (tendencia and not stopLoss and not stopWin):
 	hold=True
 else: hold=False
 
