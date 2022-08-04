0bf469f6d8803995584f7298fc112aa2df9e1d54
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-08-04 20:51:59 -0300

    Ej1

---
+++ Ej1.py
@@ -0,0 +1,17 @@
+smaFast=100
+smaSlow=98
+buyPrice=106
+price=128
+
+tendencia=smaFast>smaSlow*1.02
+stopLoss=price<buyPrice*0.95
+stopWin=price>buyPrice*1.2
+
+if (tendencia and not stopLoss and not stopWin):
+	hold="Sigo invertido"
+elif (stopLoss==True):
+	hold="Salida por stopLoss"
+else: 
+	hold="Salida por toma de ganancias"
+
+print (hold)	
