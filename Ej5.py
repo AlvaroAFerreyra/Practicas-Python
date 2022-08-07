d185e445ab5ea86b211364cab8dccb10b4d3fdd7
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-08-07 17:12:35 -0300

    Ej5

--- Ej5.py
+++ Ej5.py
@@ -1,8 +1,14 @@
 activos1=["ALUA","BMA","BYMA","CEPU","COME","CRES","CVH","EDN","GGAL","MIRG"]
 activos2=["PAM","SUPV","TECO2","TGNNO4","TGSU2","TRAN","TXAR","VALO","YPF"]
-
 activos1.extend(activos2)
 totalActivos=activos1
 
-print(totalActivos)
-print(len(totalActivos))
+activoBuscado=input("Ingrese el activo buscado: ").upper()
+
+if (activoBuscado in totalActivos):
+    print(f"El activo {activoBuscado} se encuentra dentro de la variable")
+else:
+    print(f"El activo {activoBuscado} no se encuentra en la lista")    
+
+
+
