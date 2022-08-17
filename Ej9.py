95bc4155d05e21f42f25501ee082be8e68878f99
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-08-17 19:34:25 -0300

    Ej9

--- Ej9.py
+++ Ej9.py
@@ -4,4 +4,14 @@ cierresSPY = []
 for fila in data:
 	cierresSPY.append(fila[4])
 
-print(cierresSPY[0:10])	
+count = 0
+for i in range(len(cierresSPY)):
+	try:
+		gap = float(cierresSPY[i])/float(cierresSPY[i+1])>1.05
+		if gap:
+			count+=1	
+	except:
+		continue	
+
+print(count)
+print(len(cierresSPY))
