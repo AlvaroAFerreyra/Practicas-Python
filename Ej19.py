e80fa47c5e586ea0d10d50a1f304f12b77fc165a
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-08-28 12:35:34 -0300

    Ej19

--- Ej19.py
+++ Ej19.py
@@ -1,11 +1,21 @@
 import csv
 data = csv.reader(open('estado_resultados.csv'),delimiter=';')
 balances = []
-empresas = ["FB","AMZN","AAPL","NFLX","GOOGL"]
-search = {}
+empresas = []
 
 for fila in data:
-	if fila[0] in empresas:
-		search[fila[0]] = False
+	if fila[0] != "symbol":
+		balances.append(fila)
 
-print(search)		
+for empresa in balances:
+	try:
+		ratio = int(empresa[21])/int(empresa[19]) < 0.5
+	except:
+		ratio = False	
+	try:
+		rd = int(empresa[3]) > 5000000000000 
+	except:
+		rd = False	
+	if ratio and rd and empresa[1]=="anual":
+		print(f"{empresa[0]} ratio:{ratio} R+D:{rd}")
+
