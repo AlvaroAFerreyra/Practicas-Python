00b05a32ff1c8648e94ff1da4d73a8c467422d56
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-08-29 19:34:47 -0300

    Ej19

--- Ej19.py
+++ Ej19.py
@@ -2,10 +2,14 @@ import csv
 data = csv.reader(open('estado_resultados.csv'),delimiter=';')
 balances = []
 empresas = []
+TF = {}
 
 for fila in data:
 	if fila[0] != "symbol":
 		balances.append(fila)
+
+for i in balances:
+	TF[i[0]] = False
 
 for empresa in balances:
 	try:
@@ -13,9 +17,10 @@ for empresa in balances:
 	except:
 		ratio = False	
 	try:
-		rd = int(empresa[3]) > 5000000000000 
+		rd = int(empresa[3]) > 5000000000 
 	except:
 		rd = False	
-	if ratio and rd and empresa[1]=="anual":
-		print(f"{empresa[0]} ratio:{ratio} R+D:{rd}")
+	if ratio and rd and empresa[1]=="anual" and TF[empresa[0]]==False:
+		print(empresa[0])
+		TF[empresa[0]] = True	
 
