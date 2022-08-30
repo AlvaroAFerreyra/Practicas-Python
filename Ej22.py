68b8f841486eded5b3f3f14b09abcedbb5d7bc45
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-08-29 22:52:08 -0300

    Ej22

--- Ej22.py
+++ Ej22.py
@@ -1,6 +1,21 @@
 import csv
 data = csv.reader(open('estado_resultados.csv'),delimiter=';')
 balances = []
+limit = {}
+lista_id = []
 
-for fila in data:
-	print(fila[0])
+for empresa in data:
+	limit[empresa[0]] = False
+	balances.append(empresa)
+
+balances.pop(0)
+
+for search in balances:
+	if limit[search[0]]==False and search[1]=="anual":
+		try:
+			lista_id.append(int(search[3]))	
+			limit[search[0]]=True		
+		except:
+			continue
+
+print(sum(lista_id)/len(lista_id)/1000000)		
