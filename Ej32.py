1dea1c182c50939fb11586727516a26955c32dda
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-09 14:10:19 -0300

    Ej32

---
+++ Ej32.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+baseFilter = data[(data['timestamp']>"2017-12-31") & (data['timestamp']<"2019-01-01")]
+
+baseFilter['max_intraDay'] = (((baseFilter.high/baseFilter.open)*100)-100)
+
+baseOrder = baseFilter.sort_values(by='max_intraDay',ascending=False)
+
+print(baseOrder)
