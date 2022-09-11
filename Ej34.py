361927d0dd13263dc0a85fc7a20f60349c6e9b86
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-10 21:53:41 -0300

    Ej34

---
+++ Ej34.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+data['max_intraDay'] = round(((data.high/data.open)*100)-100, 2)
+
+data.drop(["open","high","low","close","adjusted_close",'volume'],axis=1,inplace=True)
+
+baseOrdenada = (data.sort_values(by='max_intraDay', ascending=False)).head(10)
+
+print(baseOrdenada)
