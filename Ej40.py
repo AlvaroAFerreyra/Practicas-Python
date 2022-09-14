a97940b0871a44baf6a50b1630633597dbb04f2c
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-14 20:21:03 -0300

    Ej40

--- Ej40.py
+++ Ej40.py
@@ -1,1 +1,14 @@
 import pandas as pd
+data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+newTable = pd.DataFrame()
+
+newTable['fecha'] = data.timestamp
+newTable['apertura'] = data.open
+newTable['cierre'] = data.close
+newTable['movIntraDay'] = ((data.close/data.open)-1)*100
+newTable['movIntraDayPas'] = newTable.movIntraDay.shift(-1)
+
+newTable.set_index('fecha', inplace=True)
+
+print(newTable)
