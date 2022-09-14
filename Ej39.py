83f0aa02bf9060f232930b91db9b93116c00c4a5
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-14 20:06:57 -0300

    Ej39

--- Ej39.py
+++ Ej39.py
@@ -1,3 +1,16 @@
 import pandas as pd
 data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
 
+newTable = pd.DataFrame()
+
+newTable['fecha'] = data.timestamp
+newTable['apertura'] = data.open
+newTable['cierre'] = data.close
+newTable['movIntraDay'] = data.close-data.open
+newTable['movIntraDayPas'] = newTable.movIntraDay.shift(-1)
+
+newTable.set_index('fecha', inplace=True)
+
+
+
+print(newTable)
