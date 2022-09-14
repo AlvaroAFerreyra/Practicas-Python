468e796f113361809ffba6fcffc152a7d62682b1
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-13 22:55:11 -0300

    Ej37

--- Ej37.py
+++ Ej37.py
@@ -1,2 +1,12 @@
 import pandas as pd
 data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+nuevaTabla = pd.DataFrame()
+
+nuevaTabla['fecha'] = data.timestamp
+nuevaTabla['volumen'] = data.volume
+nuevaTabla['difVolume'] = data['volume'].diff(-1)
+
+nuevaTabla.set_index('fecha', inplace=True)
+
+print(nuevaTabla)
