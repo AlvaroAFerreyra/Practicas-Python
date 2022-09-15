dc06d88bcec4a089313e1509ae9589b6e999f650
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-14 22:15:11 -0300

    Ej40

--- Ej40.py
+++ Ej40.py
@@ -8,6 +8,7 @@ newTable['apertura'] = data.open
 newTable['cierre'] = data.close
 newTable['movIntraDay'] = ((data.close/data.open)-1)*100
 newTable['movIntraDayPas'] = newTable.movIntraDay.shift(-1)
+newTable['variacion'] = abs(newTable.movIntraDay) > abs(newTable.movIntraDayPas)
 
 newTable.set_index('fecha', inplace=True)
 
