7378eac201fa02f274a355cfa7fcf49e7f2b591f
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-15 22:56:09 -0300

    Ej42_Pandas

---
+++ Ej42.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+data.drop(['volume','high','low'], axis=1, inplace=True)
+
+data['gap'] = data.open/data.close.shift(-1)
+
+data['varPorcAdjClose'] = round((data.adjusted_close.pct_change(-5)-1)*100, 2)
+
+print(data.dropna())
+
