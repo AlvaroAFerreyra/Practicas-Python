beaef05f0106a676a8d83daff907f78874bf1e0b
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-17 23:25:49 -0300

    Ej42

--- Ej42.py
+++ Ej42.py
@@ -5,7 +5,7 @@ data.drop(['volume','high','low'], axis=1, inplace=True)
 
 data['gap'] = data.open/data.close.shift(-1)
 
-data['varPorcAdjClose'] = round((data.adjusted_close.pct_change(-5)-1)*100, 2)
+data['varPorcAdjClose'] = round((data.adjusted_close.pct_change(5)-1), 2)
 
 print(data.dropna())
 
