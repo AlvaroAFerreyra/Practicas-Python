b25980e915452dc85aff5d821d209d66792ec78e
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-20 21:31:38 -0300

    Ej45_Pandas

---
+++ Ej45_Pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+data.drop(['volume','high','low'], axis=1, inplace=True)
+
+data['gap'] = (data.open/data.close.shift(-1)-1)*100
+
+data['varPorcAdjClose'] = ((data.adjusted_close.shift(5)/data.adjusted_close)-1)*100
+
+filtro = data[data['gap'] > 1 & data['varPorcAdjClose' > 0]]
+
+print(filtro)
