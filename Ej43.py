d7d7e564c6896adfe0cb54d2d419cd026972f5fd
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-20 21:04:16 -0300

    Ej43

--- Ej43.py
+++ Ej43.py
@@ -7,16 +7,10 @@ data['gap'] = (data.open/data.close.shift(-1)-1)*100
 
 data['varPorcAdjClose'] = ((data.adjusted_close.shift(5)/data.adjusted_close)-1)*100
 
+filtro = data[data['gap'] > 1]
+
+print(filtro)
 
 
-newTable = pd.DataFrame()
-
-for i in data['gap']:
-	if i > 1:
-			newTable['fecha'] = data.timestamp
-			newTable['apertura'] = data.open
-			newTable['cierre'] = data.close
-			newTable['gap'] = data.gap
 	
 
-print(newTable)		
