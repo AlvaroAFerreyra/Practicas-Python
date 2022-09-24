df60851543ab2f29d2d1a458279f35842e7bc2a5
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-23 21:11:39 -0300

    Ej49_Pandas

--- Ej49_Pandas.py
+++ Ej49_Pandas.py
@@ -1,2 +1,15 @@
 import pandas as pd
 data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+fast = 10
+slow = 20
+
+data = data[::-1]
+
+data.drop(['open','close','high','low','volume'], axis=1, inplace=True)
+
+data['mediLenta'] = data.adjusted_close.ewm(span=slow, adjust=False).mean()
+
+data['mediRapida'] = data.adjusted_close.ewm(span=fast, adjust=False).mean()
+
+print(data)
