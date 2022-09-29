48c478d839ed88e0324be90ba785d9f90ca6071f
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-28 21:35:53 -0300

    Ej53_Pandas

--- Ej53_Pandas.py
+++ Ej53_Pandas.py
@@ -1,2 +1,16 @@
 import pandas as pd
 data = pd.read_excel('AAPL.xlsx',sheet_name='Hoja1')
+
+data.drop(['open','close','high','low','volume'],axis=1,inplace=True)
+
+data = data[::-1]
+
+newTable = pd.DataFrame()
+
+newTable = data[(data['timestamp'] > '2007-12-31') & (data['timestamp'] < '2009-01-01')]
+		
+newTable['difIntraDay'] = newTable.adjusted_close / newTable.adjusted_close.shift(1)
+
+newTable['yield'] = (newTable.difIntraDay.cumprod()-1)*100
+
+print(newTable[-1:]['yield'])
