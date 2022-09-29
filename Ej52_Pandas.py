c31d637c558bad03e647016af0dc59a2f6ba60da
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-28 21:23:00 -0300

    Ej52_Pandas

--- Ej52_Pandas.py
+++ Ej52_Pandas.py
@@ -5,8 +5,12 @@ data.drop(['open','close','high','low','volume'],axis=1,inplace=True)
 
 data = data[::-1]
 
-for i in range(len(data)):
-	if data.loc[i, 'timestamp'] > '2007-12-31' and data.loc[i, 'timestamp'] < '2009-01-01':
-		
+newTable = pd.DataFrame()
 
-print(data)
+newTable = data[(data['timestamp'] > '2007-12-31') & (data['timestamp'] < '2009-01-01')]
+		
+newTable['difIntraDay'] = newTable.adjusted_close / newTable.adjusted_close.shift(1)
+
+newTable['yield'] = (newTable.difIntraDay.cumprod()-1)*100
+
+print(newTable)
