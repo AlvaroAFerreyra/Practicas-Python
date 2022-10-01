0174ede00361811863f0bfe5a9eb314a4bd519d5
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-30 22:39:14 -0300

    Ej56_Pandas

--- Ej56_Pandas.py
+++ Ej56_Pandas.py
@@ -3,13 +3,17 @@ import datetime as dt
 
 data = pd.read_excel('AAPL.xlsx')
 
+newTable = pd.DataFrame()
+
 data.set_index('timestamp', inplace=True)
 
 data.drop(['open','close','volume','high','low'],axis=1,inplace=True)
 
-data = data.sort_values(by='timestamp',ascending=True)
+data = data.sort_values('timestamp',ascending=True)
 
-data['rendimiento%'] = data.adjusted_close.pct_change()*100
+data['rendimiento'] = data.adjusted_close.pct_change()*100
 
-print(data)
+newTable = data.rendimiento.groupby(data.timestamp.dt.year).prod().to_frame()
+
+print(newTable)
 
