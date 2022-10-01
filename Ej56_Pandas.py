8bcffd63c3c70bb3c18de27be59d732ea7d39617
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-09-30 22:11:06 -0300

    Ej56_Pandas

---
+++ Ej56_Pandas.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import datetime as dt
+
+data = pd.read_excel('AAPL.xlsx')
+
+data.set_index('timestamp', inplace=True)
+
+data.drop(['open','close','volume','high','low'],axis=1,inplace=True)
+
+data = data.sort_values(by='timestamp',ascending=True)
+
+data['rendimiento%'] = data.adjusted_close.pct_change()*100
+
+print(data)
+
