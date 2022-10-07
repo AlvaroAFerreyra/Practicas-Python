82b6b5a969771b4b9094cc9632337a0d54e409c2
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-06 21:22:57 -0300

    Ej63_Pandas

--- Ej63_Pandas.py
+++ Ej63_Pandas.py
@@ -21,6 +21,10 @@ newTable['medio'] = data.medio.resample('D').mean().to_frame()
 
 newTable['OP'] = (newTable.maximo - newTable.minimo) / newTable.medio *100
 
+newTable = newTable.sort_values('OP', ascending=False)
+
+
+print(newTable.head(5))
 
 print(newTable.loc[newTable.OP > 5].OP.count())
 
