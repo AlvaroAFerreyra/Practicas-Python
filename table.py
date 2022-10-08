185789c809da2386f8929e19396c597c9d29167c
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-07 23:58:26 -0300

    tabla

---
+++ table.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import datetime as dt
+
+activos = ["AAPL","SPY","QQQ"]
+
+listaVariaciones = []
+
+for activo in activos:
+	tabla = pd.read_excel(str(activo)+'.xlsx')
+	tabla = sort_values('timestamp', ascending=True)
+	tabla['change_pct'] = tabla['adjusted_close'].pct_change()*100
+	tabla.set_index('timestamp', inplace=True)
+	listaVariaciones.append(tabla['change_pct'])
+
+tablaFinal = pd.concat(listaVariaciones, axis=1).dropna()
+tablaFinal.columns = activos
+	
