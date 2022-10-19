07f701c822cc23276421ba2249985d74fd49ce25
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-18 23:40:04 -0300

    Ej69_Pandas

--- Ej69_Pandas.py
+++ Ej69_Pandas.py
@@ -3,16 +3,16 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 i=0
-o=0
+a=0
 
 empresas = ['BBAR','BMA','CRESY','EDN','GGAL','PAM','TEO','TGS','YPF']
 
 for empresa in empresas:
 	empresas[i] = pd.read_excel(empresa+'.xlsx')
-	"""empresas[i].set_index('timestamp', inplace=True)"""
 	i=i+1
 
-for o in range(len(empresas)):
-	empresas[o] = empresas[o][(empresas[o].timestamp > '2012-12-31') & (empresas[o].timestamp < '2014-1-1')]
-	print(empresas[o])
-	o=o+1
+for a in range(len(empresas)):
+	empresas[a] = empresas[a][(empresas[a].timestamp > '2012-12-31') & (empresas[a].timestamp < '2014-1-1')]
+	empresas[a]['varDiaria'] = empresas[0].adjusted_close.pct_change(-1)*100
+	print(empresas[a])
+	a=a+1
