73fa52e12c834e037ce568dedd1a66c7417eaa67
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-20 23:04:55 -0300

    Ej71_Pandas

---
+++ Ej71_Pandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+
+data = pd.read_excel("ADRs2013.xlsx")
+
+data.set_index('timestamp', inplace=True)
+
+rendimiento = data.mean()
+desvio = data.std()
+TLR = 0.03 / 360
+
+newTable = pd.DataFrame(index = data.columns)
+
+newTable['sharpeDiario'] = (rendimiento - TLR) / desvio
+
+newTable['sharpeAnual'] =  newTable.sharpeDiario * 250**0.5
+
+print(newTable)
