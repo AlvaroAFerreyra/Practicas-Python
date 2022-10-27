ae9e9555ed8c9b9eeb34380050b64f4a1bc2af6a
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-26 23:26:38 -0300

    Ej74_Pandas

---
+++ Ej74_Pandas.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+
+listaVar = []
+
+empresas = ['BBAR','BMA','CRESY','EDN','GGAL','PAM','TEO','TGS','YPF']
+
+for empresa in empresas:
+	tabla = pd.read_excel(empresa+'.xlsx')
+	tabla = tabla.sort_values('timestamp', ascending=True)
+	tabla['varDiaria'] = tabla.adjusted_close.pct_change()*100
+	tabla.set_index('timestamp', inplace=True)
+	listaVar.append(tabla['varDiaria'])
+
+newDF = pd.concat(listaVar, axis=1)
+
+newDF.columns = empresas
+
+newDF.dropna().to_excel("ADRsHIST.xlsx")
+
