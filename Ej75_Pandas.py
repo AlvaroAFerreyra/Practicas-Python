bf651d9ee8a39d781a5f493ac019ab12d691321f
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-27 19:47:38 -0300

    Ej75_Pandas

--- Ej75_Pandas.py
+++ Ej75_Pandas.py
@@ -2,9 +2,22 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 
+empresas = ['BBAR','BMA','CRESY','EDN','GGAL','PAM','TEO','TGS','YPF']
+
+listavn = []
+
 data = pd.read_excel("ADRsHIST.xlsx")
 data.set_index('timestamp', inplace=True)
 
-pr = data.YPF.std()-2.5
+for empresa in empresas:
 
-print(pr)
+	vn = data[empresa].quantile(0.01)
+	listavn.append(vn)
+
+grafico = pd.DataFrame(index=data.columns)
+
+grafico['varNeg'] = listavn 
+
+fig, ax=plt.subplots()
+ax.bar(grafico.index, grafico.varNeg)
+plt.show()
