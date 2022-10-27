b8f219dc5a962a8939b1582f85986e80029ef8d6
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-26 23:14:04 -0300

    Ej72_Pandas

--- Ej72_Pandas.py
+++ Ej72_Pandas.py
@@ -3,3 +3,19 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 data = pd.read_excel("ADRs2013.xlsx")
+
+data.set_index('timestamp', inplace=True)
+
+rendimiento = data.mean()
+desvio = data.std()
+TLR = 0.03 / 360
+
+newTable = pd.DataFrame(index = data.columns)
+
+newTable['sharpeDiario'] = (rendimiento - TLR) / desvio
+
+newTable['sharpeAnual'] =  newTable.sharpeDiario * 250**0.5
+
+fig, ax=plt.subplots()
+ax.bar(newTable.index, newTable.sharpeAnual)
+plt.show()
