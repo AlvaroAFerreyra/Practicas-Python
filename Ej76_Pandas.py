f19f7758ec0e58b3b844e407ea5f8bbaea316570
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-27 19:55:04 -0300

    Ej76_Pandas

---
+++ Ej76_Pandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+
+empresas = ['BBAR','BMA','CRESY','EDN','GGAL','PAM','TEO','TGS','YPF']
+
+listavn = []
+
+data = pd.read_excel("ADRsHIST.xlsx")
+data.set_index('timestamp', inplace=True)
+
+for empresa in empresas:
+
+	vn = data[empresa].quantile(0.999)
+	listavn.append(vn)
+
+fig, ax=plt.subplots()
+ax.bar(empresas, listavn)
+plt.show()
