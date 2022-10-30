527d021858fc7bf698b09e91b4cc9f7855759029
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-30 12:03:12 -0300

    Ej79_Pandas

--- Ej79_Pandas.py
+++ Ej79_Pandas.py
@@ -2,9 +2,6 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 
-empresas = ['BBAR','BMA','CRESY','EDN','GGAL','PAM','TEO','TGS','YPF']
-
-listavn = []
 
 data = pd.read_excel("ADRsHIST.xlsx")
 data.set_index('timestamp', inplace=True)
