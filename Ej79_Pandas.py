217c22e17e3a79941a941497aa03afa5629baf65
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-30 12:34:20 -0300

    Ej79_Pandas

--- Ej79_Pandas.py
+++ Ej79_Pandas.py
@@ -2,8 +2,13 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 
+empresas = ['BBAR','BMA','CRESY','EDN','GGAL','PAM','TEO','TGS','YPF']
 
 data = pd.read_excel("ADRsHIST.xlsx")
 data.set_index('timestamp', inplace=True)
 
-print(data.corr(method="pearson").round(2))
+data = data.corr(method="pearson").round(2)
+
+for empresa in empresas:
+	data = data.sort_values(empresa, ascending=True)
+	print(f"El activo menos correlacionado con {empresa} es {data[empresa].idxmin()}")
