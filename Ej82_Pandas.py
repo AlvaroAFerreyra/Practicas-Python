79761d236fbb9a0507becd8853cd3b5cec1ef670
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-10-31 23:31:48 -0300

    Ej82_Pandas

---
+++ Ej82_Pandas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_excel('SPY.xlsx')
+data.set_index('timestamp', inplace=True)
+
+
+graf2014 = data.loc[(data.index >= "2014-01-01") & (data.index <= "2014-12-31")]
+graf2015 = data.loc[(data.index >= "2015-01-01") & (data.index <= "2015-12-31")]
+
+
+fig, gf1=plt.subplots(figsize=(10,4))
+gf1.plot(graf2014.index, graf2014.adjusted_close, color='b')
+gf2 = gf1.twiny()
+gf2.plot(graf2015.adjusted_close, color='r')
+plt.show()
+
