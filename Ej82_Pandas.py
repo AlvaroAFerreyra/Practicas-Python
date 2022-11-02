fdd1125721f2a86c672ac9fc9e8c8b88271f84b1
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-01 23:00:49 -0300

    Ej82_Pandas

--- Ej82_Pandas.py
+++ Ej82_Pandas.py
@@ -10,8 +10,9 @@ graf2015 = data.loc[(data.index >= "2015-01-01") & (data.index <= "2015-12-31")]
 
 
 fig, gf1=plt.subplots(figsize=(10,4))
-gf1.plot(graf2014.index, graf2014.adjusted_close, color='b')
+gf1.plot(graf2014.index, graf2014.adjusted_close, color='g')
 gf2 = gf1.twiny()
 gf2.plot(graf2015.adjusted_close, color='r')
+plt.legend(['2014','2015'], loc='lower right')
 plt.show()
 
