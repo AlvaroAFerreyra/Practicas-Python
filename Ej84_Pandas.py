aec514c1d4752811114f794bbe02bce39113b7e6
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-02 22:50:17 -0300

    Ej84_Pandas

--- Ej84_Pandas.py
+++ Ej84_Pandas.py
@@ -9,7 +9,7 @@ años = ['2014','2015','2016','2017','2018','2019']
 precios = {}
 
 for año in años:
-	precios[año] = data.adjusted_close.loc[(data.index >= str(año)) & (data.index < str(int(año)+1))]
+	precios[año] = (data.adjusted_close.loc[(data.index >= año) & (data.index < str(int(año)+1))]).head(250)
 	precios[año] = precios[año].reset_index(drop=True)
 	line = plt.plot(precios[año], lw=1)
 
