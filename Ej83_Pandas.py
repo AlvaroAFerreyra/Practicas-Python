74c22060d77ebd6e653b6a20755839e3ac2b9ab2
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-02 22:24:42 -0300

    Ej83_Pandas

--- Ej83_Pandas.py
+++ Ej83_Pandas.py
@@ -11,13 +11,15 @@ precios = {}
 desde = input("Ingrese el año desde el que quiere comparar: ")
 hasta = input("Ingrese el año hasta el que quiere comparar: ")
 
+plt.style.use('fivethirtyeight')
+
 for i in range(int(desde), int(hasta)+1):
 	años.append(i)
 
 for año in años:
 	precios[año] = data.adjusted_close.loc[(data.index >= str(año)) & (data.index < str(año+1))]
 	precios[año] = precios[año].reset_index(drop=True)
-	line = plt.plot(precios[año])
+	line = plt.plot(precios[año], lw=1)
 
 plt.legend(años, loc='upper left')
 plt.show()	
