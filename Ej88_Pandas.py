9cfd539cd61f127a816c90dd492fddc9c0c6d5d5
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-07 23:12:19 -0300

    Ej88_Pandas

--- Ej88_Pandas.py
+++ Ej88_Pandas.py
@@ -5,18 +5,17 @@ data = pd.read_excel("AAPL.xlsx")
 data = data.sort_values('timestamp', ascending=True)
 data.set_index('timestamp', inplace=True)
 
-
+precios = {}
 años = [['2014','2015','2016'],['2017','2018','2019']]
 
 fig, ax = plt.subplots(nrows=len(años), ncols=len(años[0]), figsize=(10,5))
 
 for fila in range(len(años)):
 	for columna in range(len(años[0])):
-		precios[años[fila][columna]] = data.adjusted_close.loc[(data.index >= años[fila][columna]) & (data.index < años[fila][columna]+1]
-		precios[años[fila][columna]].reset_index(drop=True)
-		ax[fila][columna] = data.adjusted_close.loc([data.index == años[fila][columna]])
+		precios[años[fila][columna]] = data.adjusted_close.loc[(data.index >= años[fila][columna]) & (data.index < str(int(años[fila][columna])+1))]
+		serie = precios[años[fila][columna]].reset_index(drop=True)
 		ax[fila][columna].plot(serie)
-		ax[fila][columna].legend([year], loc='lower right')
+		ax[fila][columna].legend([años[fila][columna]], loc='lower right')
 		ax[fila][columna].set_xticks([])
 
 plt.show()
