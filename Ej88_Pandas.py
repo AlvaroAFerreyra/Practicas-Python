b92281ace0f4d4367e83cf91bfea24d80f655ee8
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-09 20:02:48 -0300

    Ej88_Pandas

--- Ej88_Pandas.py
+++ Ej88_Pandas.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 data = pd.read_excel("AAPL.xlsx")
 data = data.sort_values('timestamp', ascending=True)
 data.set_index('timestamp', inplace=True)
-data['var'] = data.adjusted_close - data.adjusted_close.shift()
-
 
 variaciones = {}
 años = ['2014','2015','2016','2017','2018','2019']
@@ -14,9 +12,12 @@ años = ['2014','2015','2016','2017','2018','2019']
 fig, ax = plt.subplots(figsize=(10,5))
 
 for año in años:
-	variaciones[año] = data['acum'].loc[(data.index >= año) & (data.index < str(int(año)+1))]
-	serie = variaciones[año].reset_index(drop=True)
-	ax.plot(serie)
+	variaciones[año] = data.loc[(data.index >= año) & (data.index < str(int(año)+1))]
+	variaciones[año]['pct_ch'] = variaciones[año]['adjusted_close'].pct_change()+1
+	variaciones[año]['rendimiento'] = variaciones[año]['pct_ch'].cumprod()*100
+	variaciones[año].dropna(inplace=True)
+	variaciones[año] = variaciones[año].reset_index(drop=True)
+	ax.plot(variaciones[año]['rendimiento'])
 	ax.legend(años, loc='lower right')
 
 
