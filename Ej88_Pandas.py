92ff30187bd8a7ab5d537a140830664e30780b3e
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-08 22:33:42 -0300

    Ej88_Pandas

--- Ej88_Pandas.py
+++ Ej88_Pandas.py
@@ -6,16 +6,15 @@ data = data.sort_values('timestamp', ascending=True)
 data.set_index('timestamp', inplace=True)
 
 precios = {}
-años = [['2014','2015','2016'],['2017','2018','2019']]
+años = ['2014','2015','2016','2017','2018','2019']
 
 fig, ax = plt.subplots(figsize=(10,5))
 
-for fila in range(len(años)):
-	for columna in range(len(años[0])):
-		precios[años[fila][columna]] = data.adjusted_close.loc[(data.index >= años[fila][columna]) & (data.index < str(int(años[fila][columna])+1))]
-		serie = precios[años[fila][columna]].reset_index(drop=True)
-		ax.plot(serie)
-		ax.legend([años[fila][columna]], loc='lower right')
+for año in años:
+	precios[año] = data.adjusted_close.loc[(data.index >= año) & (data.index < str(int(año)+1))]
+	serie = precios[año].reset_index(drop=True)
+	ax.plot(serie)
+	ax.legend(años, loc='lower right')
 
 
 plt.show()
