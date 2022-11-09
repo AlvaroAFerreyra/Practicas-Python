0b2c9084230cf280f745d4a57481988485c08e92
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-08 22:50:48 -0300

    Ej88_Pandas

--- Ej88_Pandas.py
+++ Ej88_Pandas.py
@@ -1,23 +1,26 @@
 import pandas as pd
+import datetime as dt
 import matplotlib.pyplot as plt
 
 data = pd.read_excel("AAPL.xlsx")
 data = data.sort_values('timestamp', ascending=True)
 data.set_index('timestamp', inplace=True)
-
-precios = {}
+data['var'] = ((data.adjusted_close.pct_change()+1)*100).dropna()
+data['acum'] = data.var + data.var.shift().dropna()
+print(data)
+variaciones = {}
 años = ['2014','2015','2016','2017','2018','2019']
 
-fig, ax = plt.subplots(figsize=(10,5))
+"""fig, ax = plt.subplots(figsize=(10,5))
 
 for año in años:
-	precios[año] = data.adjusted_close.loc[(data.index >= año) & (data.index < str(int(año)+1))]
-	serie = precios[año].reset_index(drop=True)
+	variaciones[año] = data.var.loc[(data.index >= año) & (data.index < str(int(año)+1))]
+	serie = variaciones[año].reset_index(drop=True)
 	ax.plot(serie)
 	ax.legend(años, loc='lower right')
 
 
 plt.show()
-
+"""
 
 
