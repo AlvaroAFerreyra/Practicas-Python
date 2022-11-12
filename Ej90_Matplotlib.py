080c214cc959eb47f1fd2eff6887a4b5c8a87465
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-12 19:09:49 -0300

    Ej90_Matplotlib

--- Ej90_Matplotlib.py
+++ Ej90_Matplotlib.py
@@ -10,15 +10,15 @@ data['intra'] = (data.adjusted_close / data.open-1)*100
 data.drop(['high','low','volume','close','open','adjusted_close'], axis=1, inplace=True)
 
 años = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020']
-gapsAnualizados = data.gap.resample('Y').mean()
-intraAnualizado = data.intra.resample('Y').mean()
+gapsAnualizados = data.gap.groupby(data.index.year).mean()
+intraAnualizado = data.intra.groupby(data.index.year).mean()
 
 print(gapsAnualizados)
 
 fig, ax = plt.subplots(figsize = (10,5))
 ax.plot(gapsAnualizados)
 ax.plot(intraAnualizado)
-ax.legend(años, loc="upper left")
+ax.legend(['Gaps','Intradiarios'], loc="lower right")
 
 
 
