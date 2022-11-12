582b79cb2b03933da7c129d38a60c28369d846f6
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-12 19:05:18 -0300

    Ej90_Matplotlib

--- Ej90_Matplotlib.py
+++ Ej90_Matplotlib.py
@@ -5,21 +5,20 @@ import matplotlib.pyplot as plt
 data = pd.read_excel('AAPL.xlsx')
 data = data.sort_values('timestamp', ascending=True)
 data.set_index('timestamp', inplace=True)
-data.drop(['high','low','volume','close'], axis=1, inplace=True)
+data['gap'] = (data.open / data.adjusted_close.shift(1)-1)*100
+data['intra'] = (data.adjusted_close / data.open-1)*100
+data.drop(['high','low','volume','close','open','adjusted_close'], axis=1, inplace=True)
 
-serie = {}
-años = ['2015','2016','2017','2018','2019']
-fig, ax = plt.subplots(figsize=(10,5))
+años = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020']
+gapsAnualizados = data.gap.resample('Y').mean()
+intraAnualizado = data.intra.resample('Y').mean()
 
-for año in años:
-	serie[año] = data.loc[(data.index >= año) & (data.index < str(int(año)+1))].dropna().copy()
-	serie[año]['gap'] = (serie[año]['open'] / serie[año]['adjusted_close'].shift()-1)*100
-	serie[año]['intra'] = (serie[año]['adjusted_close'] / serie[año]['open']-1)*100
-	print(serie[año]['gap'].resample('Y').mean().to_frame())
-	print(serie[año]['intra'].resample('Y').mean().to_frame())
-	ax.plot(serie[año]['gap'].resample('Y').mean().to_frame())
-	ax.plot(serie[año]['intra'].resample('Y').mean().to_frame())
-	ax.legend(años, loc="upper left")
+print(gapsAnualizados)
+
+fig, ax = plt.subplots(figsize = (10,5))
+ax.plot(gapsAnualizados)
+ax.plot(intraAnualizado)
+ax.legend(años, loc="upper left")
 
 
 
