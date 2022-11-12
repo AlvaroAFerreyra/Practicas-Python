5c3aa7cebac05f721af4a35397776755020f9f88
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-12 20:39:51 -0300

    Ej90_Matplotlib

--- Ej91_Matplotlib.py
+++ Ej91_Matplotlib.py
@@ -6,16 +6,19 @@ data = pd.read_excel("AAPL.xlsx")
 data = data.sort_values('timestamp', ascending=True)
 data.set_index('timestamp', inplace=True)
 data['intraDay'] = (data.close / data.open-1)*100
+data['volatilidad'] = (data.adjusted_close/data.adjusted_close.shift(1)-1).rolling(250).std()*250**0.5*100
 
 maxIntra = data.intraDay.groupby(data.index.year).max()
 minIntra = data.intraDay.groupby(data.index.year).min()
-volatilidad = data.adjusted_close.groupby(data.index.year).std()*250**0.5
+volatilidadMax = data.volatilidad.groupby(data.index.year).max()
+volatilidadMin = data.volatilidad.groupby(data.index.year).min()
 
 fig, ax = plt.subplots(ncols=2, figsize=(10,5))
 ax[0].plot(maxIntra)
 ax[0].plot(minIntra)
 ax[0].legend(['maxIntra','minIntra'], loc='upper left')
-ax[1].plot(volatilidad)
-ax[1].legend(['volatilidad'], loc='upper right')
+ax[1].plot(volatilidadMax)
+ax[1].plot(volatilidadMin)
+ax[1].legend(['volatilidadMax','volatilidadMin'], loc='upper right')
 
 plt.show()
