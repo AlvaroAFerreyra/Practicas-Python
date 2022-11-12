ab863d4ead57ceef8f0202e1b7d0a9cd7b495270
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-12 19:31:54 -0300

    Ej90_Matplotlib

--- Ej90_Matplotlib.py
+++ Ej90_Matplotlib.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 
 data = pd.read_excel('AAPL.xlsx')
 data = data.sort_values('timestamp', ascending=True)
-data['gap'] = (data.open / data.adjusted_close.shift(1)-1)*100
-data['intra'] = (data.adjusted_close / data.open-1)*100
+data['gap'] = (data.open / data.close.shift(1)-1)*100
+data['intra'] = (data.close / data.open-1)*100
 cleanTable = data.drop(['high','low','volume','close','open','adjusted_close'], axis=1)
 cleanTable = cleanTable.dropna().set_index('timestamp')
 
-años = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020']
-gapsAnualizados = cleanTable['gap'].groupby(cleanTable.index.year).mean()
-intraAnualizado = cleanTable['intra'].groupby(cleanTable.index.year).mean()
+gapsAnualizados = cleanTable.gap.groupby(cleanTable.index.year).mean()
+intraAnualizado = cleanTable.intra.groupby(cleanTable.index.year).mean()
 
 print(gapsAnualizados)
+print(intraAnualizado)
 
 fig, ax = plt.subplots(figsize = (10,5))
 ax.plot(gapsAnualizados)
