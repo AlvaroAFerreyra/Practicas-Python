22ea935faaed3ded625b42621dc81f82e6cd5e63
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-15 18:44:31 -0300

    Ej93_Matplotlib

--- Ej93_Matplotlib.py
+++ Ej93_Matplotlib.py
@@ -8,7 +8,7 @@ data.set_index('timestamp', inplace=True)
 
 data['varstd'] = data.adjusted_close.pct_change().rolling(20).std()
 data['varstd_c'] = data.adjusted_close.rolling(20).std()
-data['volatilidad'] = data.varstd * 20 **0.5
+data['volatilidad'] = data.varstd * 20 ** 0.5
 data['sma20'] = data.adjusted_close.rolling(20).mean()
 data['bollingerSup'] = data.sma20 + 2.5 * data.varstd_c
 data['bollingerInf'] = data.sma20 - 2.5 * data.varstd_c
