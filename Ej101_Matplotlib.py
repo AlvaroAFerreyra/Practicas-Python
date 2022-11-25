abfef8c98328a1abcae7eece102769b37bfe9461
AlvaroAFerreyra <alvaroaf19@gmail.com>
2022-11-24 23:26:03 -0300

    Ej101_Matplotlib

--- Ej101_Matplotlib.py
+++ Ej101_Matplotlib.py
@@ -36,15 +36,15 @@ dataAj.columns = ['open', 'high', 'low', 'close', 'volume']
 
 dataAj['gap'] = (dataAj.open / dataAj.close.shift(1)-1)*100
 dataAj['gapSup'] = dataAj.eval('gap' > porcGap)
-dataAj['gapInf'] = dataAj.eval('gap' < porcGap)
+dataAj['gapInf'] = dataAj.eval('gap' < (porcGap*-1))
 dataAj['signalSup'] = (dataAj.close*1.02).loc[dataAj.gapSup == True]
 dataAj['signalInf'] = (dataAj.close*0.98).loc[dataAj.gapInf == True]
 
 dataAj = dataAj.loc[(dataAj.index >= principio) & (dataAj.index < fin)]
-
+print(dataAj)
 plt.figure(figsize=(14,5))
 plt.plot(dataAj.close)
 """plt.plot(dataAj.index, dataAj.signalSup, '˅', markersize=10)"""
-plt.plot(dataAj.index, dataAj.signalInf, '^', markersize=10, c="r")
+plt.plot(dataAj.index, dataAj.signalInf, "^", markersize=10, c="r")
 plt.show()
 
